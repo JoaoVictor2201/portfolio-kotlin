684eedaee2c77ae1a0d8d1a007c65ccf09371d49
Joao Victor <franca.jv@hotmail.com>
2025-09-06 02:19:59 -0300

    Desafio Fumante

--- app/src/main/java/com/example/portfolio/Desafio.kt
+++ app/src/main/java/com/example/portfolio/Desafio.kt
@@ -2,6 +2,7 @@ package com.example.portfolio
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.portfolio.databinding.ActivityDesafioBinding
 
@@ -14,9 +15,38 @@ class Desafio: AppCompatActivity() {
         binding = ActivityDesafioBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        fun fumando(cigas: Int, quantosAnos: Int): Int {
+            val totalCigas = cigas * (quantosAnos * 365)
+            val vidaPerdidaMinutos = totalCigas * 10
+            val vidaPerdidaDias = vidaPerdidaMinutos / 1440
+
+            return vidaPerdidaDias
+        }
+
+        val campoCigas = binding.campoCigas
+        val campoTempo = binding.campoTempo
+        val botao = binding.btnCalcular
+
         binding.toolbarDesafio.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.btnCalcular.setOnClickListener {
+            val textoCigas = campoCigas.text.toString()
+            val textoTempo = campoTempo.text.toString()
+
+            if (textoCigas.isEmpty() || textoTempo.isEmpty()) {
+                Toast.makeText(this, "Preencha os campos!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val cigasInt = textoCigas.toInt()
+            val tempoInt = textoTempo.toInt()
+
+            val vidaPerdida = fumando(cigasInt, tempoInt)
+
+            binding.resultadoFumante.text = "Uma pessoa fumando $cigasInt cigarros por dia por $tempoInt anos perdeu $vidaPerdida dias de vida"
         }
     }
 
